3aaf3099512a7d892856bc7a24bd06e6bf232c19
unknown <ever43@gmail.com>
2021-07-23 14:17:51 +0900

    header

--- uart.h
+++ uart.h
@@ -1,4 +1,8 @@
+#ifndef UART_H
+#define UART_H
 
 int uart_init();
 void* uart_thd_run(void *arg);
 void uart_close();
+
+#endif
--- util.h
+++ util.h
@@ -1,4 +1,8 @@
+#ifndef UTIL_H
+#define UTIL_H
 
 void sleep_ms(unsigned long time);
 
 #define printfIt() printf("Call %s at %s::%05d \r\n", __FUNCTION__, __FILE__, __LINE__);
+
+#endif
